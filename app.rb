07b049cf19de8994590bef2f09c52ab716b31b45
asijaw <asija.w@yahoo.com>
2020-03-24 00:35:08 -0500

    Done.

--- app.rb
+++ app.rb
@@ -4,19 +4,18 @@ class App < Sinatra::Base
 
     get '/newteam' do
         erb :newteam
-    end 
-
-    get '/team' do
-
-        @team_name = params["name"]
+      end
+    
+      post '/team' do
+        @name = params["name"]
         @coach = params["coach"]
         @pg = params["pg"]
         @sg = params["sg"]
         @sf = params["sf"]
         @pf = params["pf"]
-        @c = params["c"]
-
+        @center = params["c"]
+    
         erb :team
-    end 
-
+      end
+    
 end
